39e544e7043365f7f32d577e6ab3bae2cc569556
刘彦玮 <coolnameismy@hotmail.com>
2014-03-13 21:46:55 +0800

    create project in github

---
+++ XinZhanPcbWeb/Controllers/AboutController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XinZhanPcbWeb.Controllers
+{
+    public class AboutController : Controller
+    {
+        //
+        // GET: /AboutUs/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Introduce()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ XinZhanPcbWeb/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XinZhanPcbWeb.Controllers
+{
+    public class OrderController : Controller
+    {
+        //
+        // GET: /Order/
+
+        public ActionResult Book()
+        {
+            return View();
+        }
+
+        public ActionResult How()
+        {
+            return View();
+        }
+    }
+}
